5d38cfc7965b870cd2825dd409ec88a6d3b2987d
blackreaper228 <ntsvetnikov@gmail.com>
2025-02-27 04:04:14 +0300

    searchbar

--- src/javascript/burgerMenu.js
+++ src/javascript/burgerMenu.js
@@ -4,7 +4,7 @@ import Q_burgerIcon from '../svg/Q_BurgerIcon.svg'
 document.addEventListener('DOMContentLoaded', () => {
   const burgerIcon = document.querySelector('.Q_BurgerIcon')
   const burgerMenu = document.querySelector('.O_BurgerMenu')
-  const burgerMenuBG = document.querySelector('.O_BurgerMenuBG')
+  const burgerMenuBG = document.querySelector('#burgerMenuBG-01')
 
   const toggleMenu = () => {
     burgerMenu.classList.toggle('active')
--- src/javascript/test.js
+++ src/javascript/test.js
@@ -1,6 +1,7 @@
 import '../index.css'
 import preloader from './preloader.js'
 import burgerMenu from './burgerMenu.js'
+import searchbarMobile from './searchbarMobile.js'
 import audioNew from './audio_new.js'
 import logo3D from './logo-3D.js'
 import goUp from './LandingGoUp.js'

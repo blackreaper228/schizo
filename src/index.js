0f8bfd76ecbe5fc3cffe8a722f42fca7f28bcab9
blackreaper228 <ntsvetnikov@gmail.com>
2025-05-27 23:52:29 +0300

    audio

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import audioMain from './javascript/audio.js'
 import logo3D from './javascript/logo-3D.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  audioMain()
-  logo3D()
+  // audioMain()
+  // logo3D()
 })

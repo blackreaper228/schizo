5cda5f48224741b43b43a3cda8b78f338731b645
blackreaper228 <ntsvetnikov@gmail.com>
2025-07-25 23:21:53 +0300

    test-2222222

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const path = require('path')
 module.exports = merge(common, {
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     publicPath: '/', // Добавляем это
     clean: true
   }
